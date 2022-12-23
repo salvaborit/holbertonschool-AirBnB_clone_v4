5d58733fdc220eb5d272ddee8c12907f8e3e18d1
Salvador Borit <salvaborit@gmail.com>
2022-12-23 16:21:11 -0300

    task2

---
+++ api_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# starts api on port 5001
+# RUN FROM ROOT DIRECTORY
+
+HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_PORT=5001 python3 -m api.v1.app
